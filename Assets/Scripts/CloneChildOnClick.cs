cf89b3d532089bd0ceef41fff6dca6a0de7e8f21
Marcus757 <Marcus757@aol.com>
2018-08-13 00:26:36 -0400

    Initial commit

---
+++ Assets/Scripts/CloneChildOnClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneChildOnClick : MonoBehaviour {
+    public Transform prefab;
+    public float xOffset;
+    private Transform child;
+
+	// Use this for initialization
+	void Start () {
+        child = transform.GetChild(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void CloneOnClick()
+    {
+        int childCount = transform.childCount;
+        Transform instance =
+            Instantiate(prefab, child.localPosition, child.rotation);
+        instance.transform.SetParent(transform);
+        instance.localScale = child.localScale;
+        instance.localPosition = new Vector3(childCount * xOffset, 0, 0);
+    }
+}
---
+++ Assets/Scripts/RotateOnButtonPressed.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnButtonPressed : MonoBehaviour {
+    private Animator anim;
+
+    public void OnButtonPressed()
+    {
+        foreach (Transform child in transform)
+        {
+            anim = child.GetComponent<Animator>();
+            anim.SetBool("isRotating", true);
+        }
+
+        Debug.Log("Rotate button pressed");
+    }
+
+    public void OnButtonReleased()
+    {
+        foreach (Transform child in transform)
+        {
+            anim = child.GetComponent<Animator>();
+            anim.SetBool("isRotating", false);
+        }
+
+        Debug.Log("Rotate button released");
+    }
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
